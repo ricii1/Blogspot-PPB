49defdd3eb8b5e0a534400bdbe13dc921e6ee75e
Junathan Richie <jonathanrichie05@gmail.com>
2026-04-01 11:25:38 +0700

    feat: add ANSI Color

--- KotlinKalkulator/main.kt
+++ KotlinKalkulator/main.kt
@@ -1,15 +1,21 @@
+// ANSI color codes
+const val RED = "\u001B[31m"
+const val GREEN = "\u001B[32m"
+const val YELLOW = "\u001B[33m"
+const val RESET = "\u001B[0m"
+
 fun main() {
 
-    println("=== KALKULATOR SEDERHANA ===")
+    println("${GREEN}=== KALKULATOR SEDERHANA ===${RESET}")
     while(true){
-        println("---Perhitungan Baru---")
+        println("${YELLOW}---Perhitungan Baru---${RESET}")
         try {
             print("Masukkan angka pertama (atau ketik 'keluar' untuk tutup): ")
             val input1 = readLine()!!
 
             if (input1.lowercase() == "keluar") {
-                println("Terima kasih telah menggunakan kalkulator!")
-                break 
+                println("${GREEN}Terima kasih telah menggunakan kalkulator!${RESET}")
+                break
             }
 
             val num1 = input1.toDouble()
@@ -26,22 +32,22 @@ fun main() {
                 "/" -> {
                     if (num2 != 0.0) num1 / num2
                     else {
-                        println("Error: Tidak bisa dibagi dengan nol!")
+                        println("${RED}Error: Tidak bisa dibagi dengan nol!${RESET}")
                         continue
                     }
                 }
                 "mod" -> num1 % num2
                 else -> {
-                    println("Error: Operator tidak valid!")
+                    println("${RED}Error: Operator tidak valid!${RESET}")
                     continue
                 }
             }
 
-            println("Hasil: $result")
+            println("${GREEN}Hasil: $result${RESET}")
         } catch (e: NumberFormatException){
-            println("Error: Input tidak valid! Harap masukkan angka, bukan huruf.")
+            println("${RED}Error: Input tidak valid! Harap masukkan angka, bukan huruf.${RESET}")
         } catch (e: Exception){
-            println("Error: Terjadi kesalahan sistem (${e.message})")
+            println("${RED}Error: Terjadi kesalahan sistem (${e.message})${RESET}")
         }
     }
 }
